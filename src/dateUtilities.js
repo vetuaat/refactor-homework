b813fb2c1e49974f3e85cf8de10c211778d76a5b
Tamás Végh <veghthomas@gmail.com>
2018-06-29 00:13:33 +0200

    Modify dateUtilities into class
    Modified dateUtilities object into a class with static methods.

--- src/dateUtilities.js
+++ src/dateUtilities.js
@@ -1,23 +1,23 @@
-'use strict'
+'use strict';
 
-const dateUtilities = {
-
-  getCurrentDayName(dateStr) {
+class dateUtilities {
+  static getCurrentDayName(dateStr) {
     const date = this.createUTCTimestamp(dateStr);
     return date.toLocaleDateString('en-EN', {weekday: 'long'});
-  },
+  }
 
-  createUTCTimestamp(date) {
+  static createUTCTimestamp(date) {
     return new Date(`${date.replace(' ', 'T')}Z`);
-  },
+  }
 
-  getFullDayWorkHours(startHour, endHour) {
-    const workDayInSeconds = this.getSecondsFromHMS(endHour) - this.getSecondsFromHMS(startHour);
-    const fullDayWorkHour = (workDayInSeconds / (60 * 60));
-    return fullDayWorkHour
-  },
-  
-  getSecondsFromHMS(stringHMS) {
+  static getFullDayWorkHours(startHour, endHour) {
+    const workDayInSeconds =
+      this.getSecondsFromHMS(endHour) - this.getSecondsFromHMS(startHour);
+    const fullDayWorkHour = workDayInSeconds / (60 * 60);
+    return fullDayWorkHour;
+  }
+
+  static getSecondsFromHMS(stringHMS) {
     let hmsArray = stringHMS.split(':');
     let seconds = 0;
     let minutes = 1;
@@ -26,16 +26,16 @@ const dateUtilities = {
       minutes *= 60;
     }
     return seconds;
-  },
+  }
 
-  getFormattedUTCDate(date) {
+  static getFormattedUTCDate(date) {
     const dateArray = date.toUTCString().split(' ');
     const dayOfMonth = dateArray[2];
     const month = dateArray[1];
     const hourWithMinute = dateArray[4].split(':');
     hourWithMinute.splice(2);
     return `${dayOfMonth} ${month} ${hourWithMinute.join(':')}`;
-  },
-};
+  }
+}
 
-module.exports = {dateUtilities}
+module.exports = {dateUtilities};

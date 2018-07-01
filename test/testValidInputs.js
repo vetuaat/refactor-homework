75454a05cbff2d6156f3a6848ae921854fbf5df2
Tamás Végh <veghthomas@gmail.com>
2018-07-01 15:15:35 +0200

    Separate test file into valid and invalid test file

--- test/testValidInputs.js
+++ test/testValidInputs.js
@@ -1,56 +1,11 @@
-'use strict';
+'use strict'
+
 const {expect} = require('chai');
 const dueDateProgram = require('../src/index');
-const {wrongDateInputs, validDateInputs, wrongTurnaroundHours, validTurnaroundHours} = require('./testInputs');
+const {validDateInputs, validTurnaroundHours} = require('./testInputs');
 const dueDateCalculator = new dueDateProgram();
 
-
 describe('Test dueDateProgram', function() {
-  describe('with invalid input, it returns an error', function() {
-    const invalidDateAssertions = [
-      {
-        date: wrongDateInputs.emptyInput,
-        turnaroundHours: validTurnaroundHours.twoHours,
-        result: 'Please check your input and use the YYYY-MM-DD HH:MM format',
-      },
-      {
-        date: wrongDateInputs.notValidFormat,
-        turnaroundHours: validTurnaroundHours.twoHours,
-        result: 'Please check your input and use the YYYY-MM-DD HH:MM format',
-      },
-      {
-        date: wrongDateInputs.weekend,
-        turnaroundHours: validTurnaroundHours.twoHours,
-        result: 'Please only submit bugs in working hours',
-      },
-      {
-        date: wrongDateInputs.afterWorkHours,
-        turnaroundHours: validTurnaroundHours.twoHours,
-        result: 'Please only submit bugs in working hours',
-      },
-      {
-        date: wrongDateInputs.notExistentDay,
-        turnaroundHours: validTurnaroundHours.twoHours,
-        result: 'Please check your input and use the YYYY-MM-DD HH:MM format',
-      },
-      {
-        date: validDateInputs.mondayHour9,
-        turnaroundHours: wrongTurnaroundHours.notEnoughTime,
-        result: 'Turnaround time must be greater than 0',
-      },
-    ];
-
-    invalidDateAssertions.forEach(({date, turnaroundHours, result}) => {
-      describe(`When called with ${date} and ${turnaroundHours}`, function() {
-        it(`should return ${result}`, function() {
-          expect(
-            dueDateCalculator.calculateDueDate(date, turnaroundHours)
-          ).to.equal(result);
-        });
-      });
-    });
-  });
-
   describe('with valid input, it returns the due date', function() {
     describe('tests contains within day, over day and over the weekend due dates', function() {
       const validDateAssertions = [
@@ -85,6 +40,11 @@ describe('Test dueDateProgram', function() {
         });
       });
     });
+
+
+
+
+    
     describe('tests contains edge cases', function() {
       it('with over month end date', function() {
         expect(

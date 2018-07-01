560bb748348daf5d58ff6860694b603cbd9bee77
Tamás Végh <veghthomas@gmail.com>
2018-07-01 17:59:45 +0200

    Add behavior descriptions to tests

--- test/testInvalidInputs.js
+++ test/testInvalidInputs.js
@@ -11,38 +11,44 @@ describe('Test dueDateProgram', function() {
       {
         date: wrongDateInputs.emptyInput,
         turnaroundHours: validTurnaroundHours.twoHours,
+        behavior: 'should throw exception if there is no date input',
         result: 'Please check your input and use the YYYY-MM-DD HH:MM format',
       },
       {
         date: wrongDateInputs.notValidFormat,
         turnaroundHours: validTurnaroundHours.twoHours,
+        behavior: 'should throw exception if given date is not a valid YYYY-MM-DD HH:MM format',
         result: 'Please check your input and use the YYYY-MM-DD HH:MM format',
       },
       {
         date: wrongDateInputs.weekend,
         turnaroundHours: validTurnaroundHours.twoHours,
+        behavior: 'should throw exception if given date is weekend',
         result: 'Please only submit bugs in working hours',
       },
       {
         date: wrongDateInputs.afterWorkHours,
         turnaroundHours: validTurnaroundHours.twoHours,
+        behavior: 'should throw exception if given date is after workhours',
         result: 'Please only submit bugs in working hours',
       },
       {
         date: wrongDateInputs.notExistentDay,
         turnaroundHours: validTurnaroundHours.twoHours,
+        behavior: 'should throw exception if given date is not existent',
         result: 'Please check your input and use the YYYY-MM-DD HH:MM format',
       },
       {
         date: validDateInputs.mondayHour9,
         turnaroundHours: wrongTurnaroundHours.notEnoughTime,
+        behavior: 'should throw exception if given turnaround time is a negative or 0 number',
         result: 'Turnaround time must be greater than 0',
       },
     ];
 
-    invalidDateAssertions.forEach(({date, turnaroundHours, result}) => {
+    invalidDateAssertions.forEach(({date, turnaroundHours, behavior, result}) => {
       describe(`When called with ${date} and ${turnaroundHours}`, function() {
-        it(`should return ${result}`, function() {
+        it(`${behavior}`, function() {
           expect(
             dueDateCalculator.calculateDueDate(date, turnaroundHours)
           ).to.equal(result);
--- test/testValidInputs.js
+++ test/testValidInputs.js
@@ -12,21 +12,25 @@ describe('Test dueDateProgram', function() {
         {
           date: validDateInputs.mondayHour9,
           turnaroundHours: validTurnaroundHours.twoHours,
+          behavior: 'should return with due date in the current day',
           result: 'Due date: Jul 02 11:00',
         },
         {
           date: validDateInputs.mondayHour9,
           turnaroundHours: validTurnaroundHours.eightHours,
+          behavior: 'should return with due date at the end of the current day',
           result: 'Due date: Jul 02 17:00',
         },
         {
           date: validDateInputs.mondayHour9,
           turnaroundHours: validTurnaroundHours.tenHours,
+          behavior: 'should return with due date at the next day if turnaround time is fall out from the current day',
           result: 'Due date: Jul 03 11:00',
         },
         {
           date: validDateInputs.fridayHour10,
           turnaroundHours: validTurnaroundHours.tenHours,
+          behavior: 'should return with due date at the next next week if there is a weekend',
           result: 'Due date: Jul 09 12:00',
         },
       ];
@@ -46,22 +50,22 @@ describe('Test dueDateProgram', function() {
 
     
     describe('tests contains edge cases', function() {
-      it('with over month end date', function() {
+      it('should return with due date at the next month if the current date is at the end of the month or the given turnaround time is more than the remainig hours in the month', function() {
         expect(
           dueDateCalculator.calculateDueDate(validDateInputs.fridayHour9, validTurnaroundHours.fourHundred)
         ).to.eql('Due date: Aug 09 17:00');
       });
-      it('with over year end date', function() {
+      it('should return with due date at the next year if the current date is at the end of the year or the given turnaround time is more than the remainig hours in the year', function() {
         expect(dueDateCalculator.calculateDueDate(validDateInputs.lastDayOfYearHour16, validTurnaroundHours.twoHours)).to.eql(
           'Due date: Jan 01 10:00'
         );
       });
-      it('from february to march', function() {
+      it('should return with the correct due date (March) if the current day is at the end of February', function() {
         expect(dueDateCalculator.calculateDueDate(validDateInputs.lastDayOfFebruaryHour16Minutes12, validTurnaroundHours.twoHours)).to.eql(
           'Due date: Mar 01 10:12'
         );
       });
-      it('leap february', function() {
+      it('should return with correct due date if it has to calculate with leap year', function() {
         expect(dueDateCalculator.calculateDueDate(validDateInputs.leapYearFebruaryHour16, validTurnaroundHours.twoHours)).to.eql(
           'Due date: Mar 02 10:00'
         );

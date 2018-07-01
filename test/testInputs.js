d48d73ffffa2c481dc5760cb8d0147e272e092f8
Tamás Végh <veghthomas@gmail.com>
2018-07-01 14:56:23 +0200

    Add new inputs

--- test/testInputs.js
+++ test/testInputs.js
@@ -7,7 +7,12 @@ const wrongDateInputs = {
 };
 
 const validDateInputs = {
-  weekdayHour9: '2018-07-02 09:00',
+  mondayHour9: '2018-07-02 09:00',
+  fridayHour9: '2018-06-01 09:00',
+  fridayHour10: '2018-07-06 10:00',
+  lastDayOfYearHour16: '2018-12-31 16:00',
+  lastDayOfFebruaryHour16Minutes12: '2018-02-28 16:12',
+  leapYearFebruaryHour16: '2020-02-28 16:00',
 };
 
 const wrongTurnaroundHours = {
@@ -16,6 +21,10 @@ const wrongTurnaroundHours = {
 
 const validTurnaroundHours = {
   twoHours: 2,
+  eightHours: 8,
+  tenHours: 10,
+  sixteenHours: 16,
+  fourHundred: 400,
 };
 
 module.exports = {wrongDateInputs, validDateInputs, wrongTurnaroundHours, validTurnaroundHours};
